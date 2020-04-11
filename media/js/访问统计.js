398e3fa71069165db8b8e11d6920f411a1493994
nzt-yzang <40187181@qq.com>
2020-04-11 09:46:59 +0800

    update from gridea: 2020-04-11 09:46:59

---
+++ media/js/访问统计.js
@@ -0,0 +1,10 @@
+<div>
+    <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
+    <span id="busuanzi_container_site_pv" style='display:none'>
+   		<sup style="color:grey">总访问量<span id="busuanzi_value_site_pv"></span>次</sup>
+    	<sup style="color:grey"><span class="post-meta-divider">|</span></sup>
+    </span>
+    <span id="busuanzi_container_site_uv" style='display:none'>
+    	<sup style="color:grey">访问人数<span id="busuanzi_value_site_uv"></span>位</sup>
+    </span>
+</div>
